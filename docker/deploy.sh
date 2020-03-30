d93ea199513d9087f864905e4f1fcb7937afbca6
josojo <josojo@hotmail.de>
2020-03-30 11:42:50 +0200

    correcting docker build statement

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -6,7 +6,7 @@ tag_name=$1
 echo "Docker login"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
 
-echo "Pushing to Docker-hub, generated from branch $TRAVIS_BRANCH with the tag $tag_name";
-docker build -t -f docker/Dockerfile gnosispm/dex-open-solver:$tag_name .;
+echo "Pushing to Docker-hub, generated from branch $TRAVIS_BRANCH";
+docker build -f docker/Dockerfile -t gnosispm/dex-open-solver:$tag_name .;
 docker push gnosispm/dex-open-solver:$tag_name;
 echo "The image has been pushed";

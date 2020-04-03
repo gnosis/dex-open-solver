c1a931d92776ce94374e8305ac3bede7b270ca36
AlexHerrmann(josojo) <josojo@hotmail.de>
2020-04-03 16:36:10 +0200

    Additional e2e testcase non-trivial-solution (#35)
    Replaces: Non-trivial-solution tests #34
    
    Does a simple introduction of non-trivial-solution tests, will no longer fail, once other PRs are merged

---
+++ tests/e2e/has-non-trival-solution/has_non_trivial_solutiono_test.py
@@ -0,0 +1,21 @@
+"""Assert that an instance has a nontrivial solution."""
+from src.token_pair_solver.solver import main
+from argparse import Namespace
+import tempfile
+import json
+
+
+def test_has_non_trivial_solution(local_instance):
+    """Asserts that passed local_instance has a nontrivial solution."""
+    solution = tempfile.NamedTemporaryFile(
+        mode='w+', delete=False, prefix='solution-', suffix='.json'
+    )
+    with open(local_instance, 'r') as fd:
+        args = Namespace(
+            instance=fd,
+            token_pair=('token0', 'token1'),
+            solution=solution,
+            xrate=None
+        )
+        solution = main(args)
+        assert any(int(order["execSellAmount"]) > 0 for order in solution["orders"])
